20648299f93ef0b392f1a909f510516448cd0972
Jowde <110619686+JudeBarcelona@users.noreply.github.com>
2024-02-25 12:30:31 -0600

    Made some simple changes

--- simonGUI.py
+++ simonGUI.py
@@ -14,6 +14,8 @@ NUMBER_OF_BLINKS = 4 #The number of time the leds blink when the payer loses
 INIT_STARTUP_FRAMEBUFFER = 200 #The number of frames the game runs before starting 
 FPS = 100 # The frames per second the game is run at
 SCREEN_SIZE = (720, 540)
+INPUT_FRAMEBUFFER = 100
+FRAME_DELAY = 400
 class Simon:
     
     
@@ -21,10 +23,10 @@ class Simon:
         self.sequence: list[Button] = [] 
         self.sequence_index = 0
         self.running = True
-        self.is_playback = True
-        self.counter = 200
-        self.lost = False
+        self.current_gamestate = 'playback'
+        self.counter = INIT_STARTUP_FRAMEBUFFER
         self.blink_count = NUMBER_OF_BLINKS
+        
         pygame.init()
         
         self.buttons = [
@@ -75,40 +77,51 @@ class Simon:
     
     def blink_leds(self):
         for button in self.buttons:
-            button.respond(100)
+            button.respond(INPUT_FRAMEBUFFER)
         
-        
-    def wait_for_press(self, mouse_pos):
-        for button in self.buttons:
-            if button.is_clicked(mouse_pos):
-                button.respond(100)
-                return button
-        return None
+    #Resets variables needed for playback  
+    def reset_playback(self):
+        self.sequence_index = 0
+        self.add_to_sequence()
+        self.counter = INIT_STARTUP_FRAMEBUFFER
+        self.current_gamestate = 'playback'
 
     def playback(self):
         if self.sequence_index >= len(self.sequence):
             self.sequence_index = 0
-            self.is_playback = False
+            self.current_gamestate = 'game'
         else:
             self.sequence[self.sequence_index].respond(100)
             self.counter = 100
             self.sequence_index += 1
 
+
+
+
+    #Checks the players input with what is next in the sequence. If the input doesn't match, the function changes the gamestate to end
+    def wait_for_press(self, mouse_pos):
+        for button in self.buttons:
+            if button.is_clicked(mouse_pos):
+                button.respond(INPUT_FRAMEBUFFER)
+                return button
+        if self.counter < FRAME_DELAY:
+            self.current_gamestate = 'loss'
+        return None
+    
     def check_input(self, pressed_button):
         correct_button = self.sequence[self.sequence_index]
         if pressed_button.color != correct_button.color:
-            self.lost = True
+            self.current_gamestate = 'loss'
         else:
             if self.sequence_index + 1 < len(self.sequence):
                 self.sequence_index += 1
             else:
                 self.reset_playback()
                 
-    def reset_playback(self):
-        self.sequence_index = 0
-        self.add_to_sequence()
-        self.counter = 100
-        self.is_playback = True
+        
+                
+            
+    
         
     def game_handler(self):
                 pos = pygame.mouse.get_pos()
@@ -118,25 +131,25 @@ class Simon:
                     if event.type == pygame.QUIT:
                         self.running = False
                         
-                    if event.type == pygame.MOUSEBUTTONDOWN and not self.is_playback:
+                    if event.type == pygame.MOUSEBUTTONDOWN and self.current_gamestate == 'game':
                         pressed_button = self.wait_for_press(pos)
-                
-                if not self.lost:
-                    if self.is_playback:
+                if self.current_gamestate == 'playback':
+
                         self.playback()
-                    elif pressed_button is not None:
+                elif self.current_gamestate == 'game':
+                    if pressed_button is not None:
                         self.check_input(pressed_button=pressed_button)
-                else:
-                    
+                
+                elif self.current_gamestate == 'loss':
                     if self.lose():
                         self.blink_count-=1
                     else:
-                        pygame.quit()
+                        self.running = False
                         
     def run(self):
         while self.running:
             if self.counter > 0:
-                self.counter-=1 
+                self.counter
             else:
                 self.game_handler()
                 
